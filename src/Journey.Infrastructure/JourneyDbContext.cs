f426faf1563a38a2d0bc83c3e5e88078354aed25
Daniel <d.soares@aluno.ifsp.edu.br>
2024-07-11 22:00:56 -0300

    Add project files.

---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Journey.Infrastructure.Entities;
+
+namespace Journey.Infrastructure
+{
+    public class JourneyDbContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\formi\\Desktop\\Workspace\\JourneyDatabase.db");
+        }
+    }
+}
